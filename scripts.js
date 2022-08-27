4972adee16e9f45dd48b635037face8031d1d85c
Nesha Whit <neshawhitmire@gmail.com>
2022-08-27 15:22:35 -0400

    fixed time deduction, game end time and added css styling

--- scripts.js
+++ scripts.js
@@ -1,9 +1,9 @@
-//need a timer for quiz-click start button timer begins
-//click start button quiz also begins
-//first question appears
-//click answer to question and next question appears
-//if answer incorrectly time is deducted 
-// all questions answered or timer reach 0 game ends
+//need a timer for quiz-click start button timer begins done
+//click start button quiz also begins done
+//first question appears done
+//click answer to question and next question appears done
+//if answer incorrectly time is deducted  done
+// all questions answered or timer reach 0 game ends 
 // add and save intials to score and collect scores
 //need to log highscores
 
@@ -11,13 +11,17 @@ var startButton = document.querySelector("#start-btn");
 var quizContainer = document.querySelector("#quiz");
 var startContainer = document.querySelector("#start");
 var timeContainer = document.querySelector("#time");
+var submitButton = document.querySelector("#submit")
+var endQuiz = document.querySelector("#end");
 var timeSpan = document.querySelector("#time-left");
 var questionH2 = document.querySelector("#title");
-var choice1 =document.querySelector("#choice1")
-var choice2 =document.querySelector("#choice2")
-var choice3 =document.querySelector("#choice3")
+var choice1 =document.querySelector("#choice1");
+var choice2 =document.querySelector("#choice2");
+var choice3 =document.querySelector("#choice3");
+var userInitials = document.querySelector("text");
 var timeInterval = 60;
-
+//var score = count (correctAnswer)
+//var setTime = 0;
 var currentIndex = 0;
 
 var quizQuestions = [
@@ -64,13 +68,33 @@ var quizQuestions = [
 function deductTime() {
     timeSpan.textContent = timeInterval;
     timeInterval--;
+    if (timeInterval === 0){
+        clearInterval(timeInterval);
+        endQuiz.style.display = "block";
+        quizContainer.style.display = "none";
+        timeContainer.style.display = "none";
+    
+    }
+   if(timeInterval < 0){
+    endQuiz.style.display = "block";
+    quizContainer.style.display = "none";
+    timeContainer.style.display = "none";
+
+    
+   }
+    
+ 
 }
+// get running time to stop at zero 
+
 
 
 function startQuiz() {
     quizContainer.style.display = "block";
     timeContainer.style.display = "block";
     startContainer.style.display = "none";
+    endQuiz.style.display = "none";
+    
 
     timeSpan.textContent = timeInterval
 
@@ -80,7 +104,7 @@ function startQuiz() {
 }
 
 function displayQuestion () {
-// if statemet to check if there is more questions to display
+// if statement to check if there is more questions to display 
 
     questionH2.textContent = quizQuestions[currentIndex].question;
     choice1.textContent = quizQuestions [currentIndex].answerChoices.a
@@ -90,6 +114,15 @@ function displayQuestion () {
     choice1.addEventListener("click", nextQuestion)
     choice2.addEventListener("click", nextQuestion)
     choice3.addEventListener("click", nextQuestion)
+    if (nextQuestion !== nextQuestion) { //figure out how to identify an empty array and go to next section 
+        // choice1.addEventListener("click", nextQuestion)
+        // choice2.addEventListener("click", nextQuestion)
+        // choice3.addEventListener("click", nextQuestion)
+        
+        endQuiz.style.display = "block";
+      
+        
+    }
 
 
 
@@ -98,20 +131,36 @@ function displayQuestion () {
 function nextQuestion (event) {
     var selectedAnswer = event.target.textContent;
     var correctAnswer = quizQuestions[currentIndex].correctAnswer;
+    
     // check if what they selected is correct
-
+    // log correct answers
     if(selectedAnswer == correctAnswer) {
-        alert("Correct!") 
+        alert("Correct! +25pts") 
+        // (results + 25)
+        
     } else {
-        alert("Wrong!")
-        (timeSpan - 5000)
+        alert("Wrong! - 20 seconds")
+        timeInterval = (timeInterval - 20)
+        
+        //figure out how to deduct time
     }
-
-
+    
+    
     currentIndex++;
     displayQuestion()
 }
 
+function collectResults(){
+var results = count(quizQuestions.question.correctAnswer)
+    console.log(userInitials)
+}
 
+//  function keepScore(correctAnswer){
+//       var results = 
+
+
+
+console.log(userInitials)
 
 startButton.addEventListener("click", startQuiz)
+submitButton.addEventListener("click", collectResults)
